d8194092cb90a7ecab16096dca8cee5bfe50c4a4
abdalhafezsayeh <abdalhafezsayeh1997@gmail.com>
2023-03-11 17:00:17 +0200

    Done Com Sucses And Error

---
+++ components/Sucses.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sucses() {
+  return (
+    <div className='container mx-auto flex justify-center'>
+        <div className=' border-2 border-yellow-200 inline-block py-1 px-10 text-yellow-500'>
+            <h1 className=''>Done Add Employee</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Sucses
