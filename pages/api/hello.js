9a690672d0d5ce2f07ae68c880da1224db95d11c
abdalhafezsayeh <abdalhafezsayeh1997@gmail.com>
2023-03-11 05:11:41 +0200

    Finsh Ui And Connect With DataBase

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import connectMongoose from "@/dadbase/conn"
+
 export default function handler(req, res) {
+  connectMongoose()
   res.status(200).json({ name: 'John Doe' })
 }
